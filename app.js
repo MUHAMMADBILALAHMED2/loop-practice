75079e0949a782db4f839a55574790281f416633
muhammadbilalahmed2 <muhammadbilalahmed434@gmail.com>
2024-03-27 00:42:53 +0500

    practice for loop

---
+++ app.js
@@ -0,0 +1,37 @@
+// Question no 01
+
+for(let i=0; i<5; i++){
+
+    console.log("hello world!");
+}
+
+// Question no 02
+ for(let i=1; i<=10; i++){
+   console.log(i);
+ }
+
+//  Question no 03
+let number=+prompt("enter a number");
+let lenght=+prompt("enter a lenght");
+
+for (i=1; i<=lenght; i++){
+    console.log(`${number} x ${i}  = ${i *number}`);
+}
+
+// Question no 04
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
